200c4cec4bd2bf9b0c7429354e5f5440397f5015
Ayush Porwal <pa9067628@gmail.com>
2026-05-02 12:38:07 +0530

    port changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import { validateEnv } from "./config/env.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
